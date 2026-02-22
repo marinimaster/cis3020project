ace3e27f30432cdb23b8ce2a64dcb4ff3320a772
marinimaster <villeda.mm02@gmail.com>
2026-02-22 17:39:54 -0600

    Changed public directory name

--- src/index.mjs
+++ src/index.mjs
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static("docs"));
 app.use(express.urlencoded( {extended: true}) );
 app.use(
     session({
