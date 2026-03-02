0a253769e5c280c8f432a352cd507a39caa36f01
marinimaster <villeda.mm02@gmail.com>
2026-03-02 14:47:30 -0600

    Implement new login page with error handling and styles

--- src/index.mjs
+++ src/index.mjs
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("."));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
