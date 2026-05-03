fc9bfe8a682553ef961e3ffe727f316aa006ce14
marinimaster <villeda.mm02@gmail.com>
2026-05-03 13:39:02 -0500

    Start TLS Process

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import bcrypt from 'bcrypt';
 import pool from './utils/db.mjs';
+import https from 'https';
+import tls from 'tls';
+import fs from 'fs';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,7 +14,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     session({
